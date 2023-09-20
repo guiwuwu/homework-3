545b9a643bfcb5518131e09905e5b4b86268b01a
guiwuwu <809666515@qq.com>
2023-09-20 15:09:59 +0800

    新建vue项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable vue/valid-template-root -->
+<template></template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
